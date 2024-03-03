33264402cf1ab7aa4de0101686007afe11fd281b
chintan224 <161326876+chintan224@users.noreply.github.com>
2024-03-03 18:20:12 +0530

    final

---
+++ team-a/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      Hello From Team A!
+    </div>
+  );
+}
+
---
+++ team-b/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      Hello From Team B!
+    </div>
+  );
+}
